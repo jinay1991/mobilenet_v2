d37e42b3d70d553583d4d36e68640d6dc5e12c48
Jinay Patel <jinay1991@gmail.com>
2020-01-24 20:44:50 +0530

    TFLiteInferenceEngine

--- tflite_inference.py
+++ tflite_inference.py
@@ -12,6 +12,35 @@ from mobilenet import decode_predictions
 from PIL import Image
 from tflite_converter import convert_mobilenet
 
+
+class TFLiteInferenceEngine(object):
+    def __init__(self):
+        tflite_model = convert_mobilenet()
+
+        self._interpreter = tf.lite.Interpreter(model_content=tflite_model)
+        self._interpreter.allocate_tensors()
+
+        self.input_details = self._interpreter.get_input_details()
+        self.output_details = self._interpreter.get_output_details()
+
+    def __call__(self, image_path, input_mean, input_std):
+        # NxHxWxC, H:1, W:2
+        height = self.input_details[0]['shape'][1]
+        width = self.input_details[0]['shape'][2]
+        img = Image.open(image_path).resize((width, height))
+
+        # add N dim
+        input_data = np.expand_dims(img, axis=0)
+
+        input_data = (np.float32(input_data) - input_mean) / input_std
+
+        self._interpreter.set_tensor(self.input_details[0]['index'], input_data)
+
+        self._interpreter.invoke()
+
+        return self._interpreter.get_tensor(self.output_details[0]['index'])
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -29,33 +58,10 @@ if __name__ == '__main__':
         help='input standard deviation')
     args = parser.parse_args()
 
-    tflite_model = convert_mobilenet()
+    tflite_inference_engine = TFLiteInferenceEngine()
 
-    interpreter = tf.lite.Interpreter(model_content=tflite_model)
-    interpreter.allocate_tensors()
+    output_data = tflite_inference_engine(args.image, args.input_mean, args.input_std)
 
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-
-    # check the type of the input tensor
-    floating_model = input_details[0]['dtype'] == np.float32
-
-    # NxHxWxC, H:1, W:2
-    height = input_details[0]['shape'][1]
-    width = input_details[0]['shape'][2]
-    img = Image.open(args.image).resize((width, height))
-
-    # add N dim
-    input_data = np.expand_dims(img, axis=0)
-
-    if floating_model:
-        input_data = (np.float32(input_data) - args.input_mean) / args.input_std
-
-    interpreter.set_tensor(input_details[0]['index'], input_data)
-
-    interpreter.invoke()
-
-    output_data = interpreter.get_tensor(output_details[0]['index'])
     top_k_results = decode_predictions(output_data)[0]
     for class_id, class_name, class_score in top_k_results:
         print("{} {} {}".format(class_id.encode("ascii"), class_name.encode("ascii"), class_score))
